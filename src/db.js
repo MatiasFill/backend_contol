33989df0558e4f47a361a16c8dcc8b117f910d76
MatiasFill <rmscontato@gmail.com>
2025-09-20 02:25:59 -0300

    refazendo o db.js

--- src/db.js
+++ src/db.js
@@ -1,3 +1,27 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+const { Pool } = pkg;
+
+// Configura o pool de conexões com suas credenciais do PostgreSQL
+const pool = new Pool({
+  user: process.env.DB_USER || "postgres",
+  host: process.env.DB_HOST || "localhost",
+  database: process.env.DB_NAME || "stock_db", // o nome do banco que você criou
+  password: process.env.DB_PASS || "rms-1907",
+  port: process.env.DB_PORT || 5432, // porta padrão do PostgreSQL
+});
+
+// Exporta o pool para ser usado nos models
+export default pool;
+
+// Se quiser usar diretamente o método query em outros arquivos:
+export const query = (text, params) => pool.query(text, params);
+
+
+
+/*
 import pg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -12,7 +36,7 @@ const pool = new Pool({
 
 export default pool; // export default para ESM
 
-
+*/
 
 /*
 const { Pool } = require('pg');
